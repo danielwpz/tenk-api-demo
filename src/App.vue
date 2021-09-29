74ed4dc9eb16f58b78a806c8d7400e3349f2b0c3
Daniel Wang <danielwpz@gmail.com>
2021-09-29 10:25:56 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,54 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <button v-on:click='login'>login</button>
+  <button v-on:click='listAll'>All NFTs</button>
+  <button v-on:click='listMine'>My NFTs</button>
+  <button v-on:click='buy'>Buy</button>
+  <button v-on:click='reveal'>Reveal</button>
+
+  <h2>Price: {{ this.price }}</h2>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// eslint-disable-next-line
+import api from '@danielwpz/tenk-api'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+  },
+  data: () => {
+    return {
+      price: '99'
+    }
+  },
+  async mounted () {
+    await api.init()
+    console.log(api.getAccountId())
+
+    // load price
+    this.price = await api.getUnitPrice()
+  },
+  methods: {
+    login () {
+      api.signIn()
+    },
+    async listAll () {
+      const nfts = await api.getNFTs(0)
+      console.log(nfts)
+    },
+    async listMine () {
+      const nfts = await api.getMyCollections()
+      console.log(nfts)
+    },
+    async buy () {
+      const nft = await api.purchase('http://localhost:8082/')
+      console.log(nft)
+    },
+    async reveal () {
+      const nft = await api.getSuccessResult()
+      console.log(nft)
+    }
   }
 }
 </script>
